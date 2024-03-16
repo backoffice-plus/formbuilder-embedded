7036bb1201687cf13ebae5580d44172cea6f0317
Julian Haupt <julian@haupt.media>
2024-03-16 16:31:45 +0100

    merge embedding modifications into repo

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,17 @@
 <template>
-
-  <header>
-    <h1 class="text-2xl p-4 text-center">Formbuilder Demo</h1>
-  </header>
-
   <main class="container mx-auto flex flex-col gap-4">
-
-    <h2 class="text-xl">1. As Formbuilder Component</h2>
     <section>
-        <FormBuilder
-            :jsonFormsRenderers="jsonFormsRenderers"
-            :tools="tools"
-            class="flex-grow"
-            @schemaUpdated="(e)=>resultFormBuilder=e"
-        />
-        <aside>
-          <FormPreview :jsonform="resultFormBuilder" />
-        </aside>
+      <FormBuilder
+          :schema="schema"
+          :jsonFormsRenderers="jsonFormsRenderers"
+          :tools="tools"
+          class="flex-grow"
+          @schemaUpdated="onSchemaUpdated"
+          :key="key"
+          :schema-only="true"
+      />
     </section>
-
-
-
-    <h2 class="text-xl">2. At JsonForms as FormbuilderRenderer (schemaOnly)</h2>
-    <section>
-      <div class="styleA flex-grow">
-        <JsonForms
-            :schema="formJson.schema"
-            :uischema="formJson.uischema"
-            :data="{}"
-            :renderers="jsonFormsRenderers"
-            @change="(e)=>resultJsonForms=e?.data"
-        />
-      </div>
-
-      <aside>
-        <FormPreview :jsonform="resultJsonForms" />
-      </aside>
-    </section>
-
   </main>
-
 </template>
 
 <style>
@@ -50,9 +22,6 @@ main > section {
   @apply
   bg-white shadow p-4 mb-8
   flex flex-row
-}
-main > section > aside {
-  @apply w-4/12 ml-4 flex flex-col
 }
 </style>
 
@@ -65,7 +34,6 @@ import {FormBuilder, defaultTools, boplusVueVanillaRenderers, formbuilderRendere
 import '@backoffice-plus/formbuilder/style.css'
 import './style.css'
 import './form.stylea.css'
-import FormPreview from "@/components/FormPreview.vue";
 
 
 const tools = [
@@ -78,45 +46,22 @@ const jsonFormsRenderers = Object.freeze([
   ...formbuilderRenderers,
 ]);
 
-const formJson = {
-  "schema": {
-    "type": "object",
-    "properties": {
-      "name": {
-        "type": "string"
-      },
-      "schema": {
-        "type": "object"
-      }
-    }
-  },
-  "uischema":  {
-    "type": "VerticalLayout",
-    "elements": [
-      {
-        "type": "Control",
-        "scope": "#/properties/name"
-      },
-      {
-        "type": "Disclosure",
-        "label": "Schema",
-        "elements": [
-          {
-            "type": "Formbuilder",
-            "scope": "#/properties/schema",
-            "options": {
-              "schemaOnly": true
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
 
 provide('formbuilder-tools', tools)
 
 const resultFormBuilder = ref();
-const resultJsonForms = ref();
+const key = ref();
+
+const schema = ref();
+
+const onSchemaUpdated = (e) => {
+  const jfString = JSON.stringify(e.schema)
+  resultFormBuilder.value = JSON.parse(jfString);
+
+  if (window.location !== window.parent.location) {
+    // only send message if we are embedded in an iframe
+    window.top.postMessage(jfString, '*');
+  }
+}
 </script>
 
